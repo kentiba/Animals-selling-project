7c2c5adcd4496bde9e1cca7667ef49cb1a5864f0
Kentiba <kentiba93@gmail.com>
2019-05-01 19:28:55 +0200

    changed some styles in about.js

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -5,15 +5,21 @@ import './about.css';
 const About = () => {
     return (
         <div className='container text-center py-5 mt-3'>
-            <h1 className='elegantshadow'>About us </h1>
-            <div className='about'>
-                <img src={Cattle} alt='Cattle_image' className='cattle_image' />
-                <p>
-                    KenSale is an online platform for selling livestock. It has
-                    been developed to allow customers to have an easy experience
-                    to search and buy cattle. We provides cattle with accurate
-                    images,weights,location and prices.
-                </p>
+            <div className='about-container'>
+                <h1 className='elegantshadow'>About us </h1>
+                <div className='about'>
+                    <img
+                        src={Cattle}
+                        alt='Cattle_image'
+                        className='cattle_image'
+                    />
+                    <p>
+                        KenSale is an online platform for selling livestock. It
+                        has been developed to allow customers to have an easy
+                        experience to search and buy cattle. We provides cattle
+                        with accurate images,weights,location and prices.
+                    </p>
+                </div>
             </div>
         </div>
     );
